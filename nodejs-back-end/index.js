e7ea1caa065ad80a03ac8cb5a250f580803628a4
Patrick Hanlan <pat@patrickhanlan.com>
2021-12-27 16:27:19 +1100

    Added static page serving and separate api route

--- nodejs-back-end/index.js
+++ nodejs-back-end/index.js
@@ -1,15 +1,22 @@
 const PORT = 8080;
+const STATIC_PATH = `${__dirname}/static`;
+const API_BASE_ROUTE = 'api';
 
 const express = require('express');
 const httpStatusCodes = require('http-status-codes');
 
 let app = express();
+app.use(express.static(STATIC_PATH));
 
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`);
 });
 
-app.get('/', (req, resp) => {
+app.get(`/${API_BASE_ROUTE}/*`, (req, resp) => {
     resp.status(httpStatusCodes.StatusCodes.OK);
     resp.send('Hello world!');
+});
+
+app.get('/*', (req, resp) => {
+    resp.sendFile(`${STATIC_PATH}/index.html`);
 });
