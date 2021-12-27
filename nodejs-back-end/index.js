e46bced9b8c8823654bb407628f16714d52025d0
Patrick Hanlan <pat@patrickhanlan.com>
2021-12-27 17:36:34 +1100

    Set up npm scripts

--- nodejs-back-end/index.js
+++ nodejs-back-end/index.js
@@ -3,7 +3,7 @@ const STATIC_PATH = `${__dirname}/static`;
 const API_BASE_ROUTE = 'api';
 
 const express = require('express');
-const httpStatusCodes = require('http-status-codes');
+const lib_hsc = require('http-status-codes');
 
 let app = express();
 app.use(express.static(STATIC_PATH));
@@ -12,11 +12,7 @@ app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`);
 });
 
-app.get(`/${API_BASE_ROUTE}/*`, (req, resp) => {
-    resp.status(httpStatusCodes.StatusCodes.OK);
-    resp.send('Hello world!');
-});
-
-app.get('/*', (req, resp) => {
-    resp.sendFile(`${STATIC_PATH}/index.html`);
+app.get(`/${API_BASE_ROUTE}*`, (req, resp) => {
+    resp.status(lib_hsc.StatusCodes.OK)
+        .send('Hello world!');
 });
