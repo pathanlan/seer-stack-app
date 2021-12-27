0446a20dfc85d9f2f0276b968cafb69bed88ebc1
Patrick Hanlan <pat@patrickhanlan.com>
2021-12-27 18:10:42 +1100

    Set up database and basic endpoint using tutorial
    Reference: https://developerhowto.com/2018/12/29/build-a-rest-api-with-node-js-and-express-js/

---
+++ nodejs-back-end/database.js
@@ -0,0 +1,30 @@
+const DBSOURCE = "db.sqlite";
+
+const lib_sqlite = require('sqlite3').verbose();
+
+let database = new lib_sqlite.Database(DBSOURCE, (err) => {
+    if(!err) {
+        console.log('Connected to the database');
+        createTestTable(database);
+    } else {
+        console.error('There was an error when connecting to the database', err);
+        throw err;
+    }
+});
+
+function createTestTable(database) {
+    database.run(`CREATE TABLE pets (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE,
+        age INTEGER,
+        CONSTRAINT name_unique UNIQUE (name)
+    )`, (err) => {
+        if(!err) {
+            const insertTemplate = 'INSERT INTO pets (name, age) VALUES (?,?)';
+            database.run(insertTemplate, ['Cocoa', 2]);
+            database.run(insertTemplate, ['Bella', 5]);
+        }
+    })
+}
+
+module.exports = database;
--- nodejs-back-end/index.js
+++ nodejs-back-end/index.js
@@ -4,12 +4,24 @@ const API_BASE_ROUTE = 'api';
 
 const express = require('express');
 const lib_hsc = require('http-status-codes');
+const database = require('./database');
 
 let app = express();
 app.use(express.static(STATIC_PATH));
 
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`);
+});
+
+app.get(`/${API_BASE_ROUTE}/pets`, (req, resp) => {
+    database.all('SELECT * FROM pets', [], (err, rows) => {
+        if(!err) {
+            resp.status(lib_hsc.StatusCodes.OK)
+                .json({ 'message': 'success', 'data': rows });
+        } else {
+            console.error('There was an error when retrieving values from the database', err);
+        }
+    });
 });
 
 app.get(`/${API_BASE_ROUTE}*`, (req, resp) => {
