64ff1644a643cfcb9b51cbb129b0d4aff46e305f
Patrick Hanlan <pat@patrickhanlan.com>
2021-12-27 16:12:25 +1100

    Added basic GET endpoint to back-end

--- nodejs-back-end/index.js
+++ nodejs-back-end/index.js
@@ -1,1 +1,15 @@
-console.log('Hello world!');
+const PORT = 8080;
+
+const express = require('express');
+const httpStatusCodes = require('http-status-codes');
+
+let app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server has started on port ${PORT}`);
+});
+
+app.get('/', (req, resp) => {
+    resp.status(httpStatusCodes.StatusCodes.OK);
+    resp.send('Hello world!');
+});
